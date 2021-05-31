694e19017bf64f2c71e773050c160de7c94b1bc4
guust ysebie <guust.ysebie@tradecom.be>
2021-05-31 22:19:34 +0200

    kj

---
+++ algorithms/1837_sum_of_digits_base_k/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <whizx_test.h>
+
+
+int sumBase(int n, int k){
+
+    return k;
+}
+
+int main(){
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+    assert_equal_int(sumBase(20,321),1);
+}
